155cec919ce8e417a57af589c982a3640732201f
Adnan Kasem <adnankasem2017@gmail.com>
2021-09-22 12:04:35 -0700

    Pokemon V1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { PokemonsContainer } from './containers/PokemonsContainer';
 
 export function App() {
+
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon2.vercel.app/'
+  })
   return (
-    <p>I am a Pokemon</p>
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonsContainer />
+      </main>
+    </ApolloProvider>
   )
 }
